e466467eef1bd0c54cd0c53b8d71fcca99456db6
paatorek <paatorek@gmail.com>
2020-03-13 07:02:29 +0100

    Add project files.

---
+++ ModuleHelper/ViewModels/Note.cs
@@ -0,0 +1,18 @@
+﻿namespace ModuleHelper.ViewModels
+{
+    public enum Note
+    {
+        C,
+        Csharp,
+        D,
+        Dsharp,
+        E,
+        F,
+        Fsharp,
+        G,
+        Gsharp,
+        A,
+        Asharp,
+        B
+    }
+}
