673b4d5dd2ca18f4a933f8014ce9d7262c687376
paatorek <paatorek@gmail.com>
2020-03-21 16:32:45 +0100

    Moved INotifyPropertyChanged implementation to ViewModel

---
+++ ModuleHelper/Models/MusicalScaleModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using ModuleHelper.ViewModels;
+
+namespace ModuleHelper.Models
+{
+    public class MusicalScaleModel
+    {
+        public string Name { get; set; }
+        public Note MainKey { get; set; }
+        public IList<Note> Notes { get; set; }
+    }
+}
