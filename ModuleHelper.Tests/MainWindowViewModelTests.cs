feee2e050ee9b05a29d880175470adfab5463b7a
paatorek <paatorek@gmail.com>
2020-09-19 16:11:09 +0200

    Added Modulo method unit tests

--- ModuleHelper.Tests/MainWindowViewModelTests.cs
+++ ModuleHelper.Tests/MainWindowViewModelTests.cs
@@ -17,10 +17,22 @@ namespace ModuleHelper.Tests
             _mainWindowViewModel = new MainWindowViewModel(_dialogService);
         }
 
-        [TestCase]
-        public void Modulo_NegativeNumberAs1stParam_PositiveResult()
+        [TestCase(-3,3,0)]
+        [TestCase(-10,2,0)]
+        [TestCase(-8,3,1)]
+        [TestCase(-2,4,2)]
+        public void Modulo_NegativeNumberAs1stParam_PositiveResult(int x, int m, int expectedResult)
         {
-            Assert.AreEqual(MainWindowViewModel.Modulo(-3, 3), 0);
+            Assert.AreEqual(MainWindowViewModel.Modulo(x, m), expectedResult);
+        }
+
+        [TestCase(8,4,0)]
+        [TestCase(9,4,1)]
+        [TestCase(0,3,0)]
+        [TestCase(1,7,1)]
+        public void Modulo_PositiveNumberAs1stParam_PositiveResult(int x, int m, int expectedResult)
+        {
+            Assert.AreEqual(MainWindowViewModel.Modulo(x, m), expectedResult);
         }
     }
 }
